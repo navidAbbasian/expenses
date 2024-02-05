ca4d89047115b04c0af9c3818142a3ab0e8f1775
navidAbbasian <abbasian.navid@gmail.com>
2024-02-05 22:07:30 +0330

    feat: set a structure for responses

---
+++ app/Traits/ExpandedResponse.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Traits;
+
+use Exception;
+use Illuminate\Http\JsonResponse;
+
+trait ExpandedResponse
+{
+    public function JSONResponse($response, $status, $message = null): JsonResponse
+    {
+        return response()
+            ->json([
+                'message' => $message,
+                'status' => $status,
+                'data' => $response,
+            ], $status);
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function messageResponse($status, $message, $response = null): JsonResponse
+    {
+        if ($message) {
+            throw new Exception('The message is required.');
+        }
+
+        return $this->JSONResponse(response: $response, status: $status, message: $message);
+    }
+
+    protected function ok($resource): JsonResponse
+    {
+        return $this->JSONResponse(response: $resource, status: 200);
+    }
+
+    protected function created($resource): JsonResponse
+    {
+        return $this->JSONResponse($resource, 201);
+    }
+
+    protected function noContent(): JsonResponse
+    {
+        return $this->JSONResponse(null, 204);
+    }
+
+    /**
+     * @throws Exception
+     */
+    protected function resourceNotFound(): JsonResponse
+    {
+        return $this->messageResponse(status: 404, message: 'Error: Resource not found');
+    }
+}
